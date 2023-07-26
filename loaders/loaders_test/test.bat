43f571dd7b85c9fef38da514ec3d7dd0737d06cd
Khalil Henoud <khalilhenoud@gmail.com>
2023-07-26 03:38:21 -0400

    redirect to use the newly created submodules

--- loaders/loaders_test/test.bat
+++
@@ -1,1 +0,0 @@
-build\Debug\loaders_test "C:\Users\khali\Documents\source\engine\media\tree_stump\tree_stump.ase" "C:\Users\khali\Documents\source\engine\media\font\FontData2.csv" "C:\Users\khali\Documents\source\engine\media\test\12.png"
